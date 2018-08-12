70a5b7e7e4d04fc9d679f0ce93a7c5d298bdbb2f
anandamayee <anandamayee.nanda@gmail.com>
2018-08-12 23:05:09 +0530

    rest

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/MongooseApp', { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/MongooseApp', { useNewUrlParser: true })
 .catch(error=>{console.log("server connection error  ",error)});
 
 module.exports = { mongoose };
